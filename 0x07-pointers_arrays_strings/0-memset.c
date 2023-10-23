ebc6660a866d6f73e3dc24c52dcce287549879e7
Sibuso <sibusomohlwayo@gmail.com>
2023-10-23 14:32:53 -0700

    Add the memset.c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,26 +2,39 @@
 #include <string.h>
 
 /**
- * -memset-Fills memory with a constant byte.
- *  @s: Pointer to the memory area to be filled 
- *  @b: The constant byte to fill the memory with.
- *  @n: The number of bytes to fill.
+ * _memset - Fills memory with a constant byte.
+ * @s: Pointer to the memory area to be filled 
+ * @b: The constant byte to fill the memory with.
+ * @n: The number of bytes to fill.
  *
- *  return: A pointer to the filled memory @s. 
+ * Return: A pointer to the filled memory @s. 
  */
-char *_memset(char *s, char b, unsigned int n); //impleamantation of the _memset
-	for (unsigned int i=0; i < n; i++) {
-		s[i] = b
-		
-	}
-      Return s;
-int main(void)     
-	char buffer[20];
-	char *result;
+char *_memset(char *s, char b, unsigned int n);
 
-	result = _memset(buffer, 'A', 10);
+int main(void) {
+    char buffer[20];
+    char *result;
 
-	printf("Buffer after _memset: %s\n", buffer);
-	printf("Result returned by _memset: %s\n", result);
+    result = _memset(buffer, 'A', 10);
 
-	return 0; 
+    printf("Buffer after _memset: %s\n", buffer);
+    printf("Result returned by _memset: %s\n", result);
+
+    return 0;
+}
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: Pointer to the memory area to be filled 
+ * @b: The constant byte to fill the memory with.
+ * @n: The number of bytes to fill.
+ *
+ * Return: A pointer to the filled memory @s. 
+ */
+char *_memset(char *s, char b, unsigned int n) {
+    for (unsigned int i = 0; i < n; i++) {
+        s[i] = b;
+    }
+    return s;
+}
+
