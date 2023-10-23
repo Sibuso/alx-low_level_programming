ca0e073d104c15d96abd4ee15c008d4e386d6f84
Sibuso <sibusomohlwayo@gmail.com>
2023-10-23 07:53:17 -0700

    Add the 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
-int main(void) { puts ("\"Programming is like building a multilingual puzzle"); return (0); }
-
+/**
+ * main - entry point for the program
+ *
+ * This function prints a specific message using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle")
+		return (0);
+}
