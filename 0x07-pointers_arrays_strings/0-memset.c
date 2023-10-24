1d2bafb4bf7eebaee0b010c712b5c8cc70d8cf38
Sibuso <sibusomohlwayo@gmail.com>
2023-10-23 18:21:49 -0700

    Add the 0-memset.c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
  * _memset - Fills memory with a constant byte.
- * @s: Pointer to the memory area to be filled 
+ * @s: Pointer to the memory area to be filled.
  * @b: The constant byte to fill the memory with.
  * @n: The number of bytes to fill.
  *
- * Return: A pointer to the filled memory @s. 
+ * Return: A pointer to the filled memory @s.
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n) {
+    unsigned int i;
+
+    for (i = 0; i < n; i++) {
+        s[i] = b;
+    }
+
+    return s;
+}
 
 int main(void) {
     char buffer[20];
@@ -21,20 +28,5 @@ int main(void) {
     printf("Result returned by _memset: %s\n", result);
 
     return 0;
-}
-
-/**
- * _memset - Fills memory with a constant byte.
- * @s: Pointer to the memory area to be filled 
- * @b: The constant byte to fill the memory with.
- * @n: The number of bytes to fill.
- *
- * Return: A pointer to the filled memory @s. 
- */
-char *_memset(char *s, char b, unsigned int n) {
-    for (unsigned int i = 0; i < n; i++) {
-        s[i] = b;
-    }
-    return s;
 }
 
